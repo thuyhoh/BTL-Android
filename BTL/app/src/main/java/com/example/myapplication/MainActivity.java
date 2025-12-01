c0147455bee8afdf8033923934d0b57314f77818
Thuyh0h <trongthuynl2004@gmail.com>
2025-12-01 23:39:52 +0700

    update

--- BTL/app/src/main/java/com/example/myapplication/MainActivity.java
+++ BTL/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Intent intent;
+    private Button pushButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        pushButton = (Button) findViewById(R.id.);
+        pushButton.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+//        intent = new Intent(MainActivity.this,)
     }
 }
